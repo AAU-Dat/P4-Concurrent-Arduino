a6314071372d16657b16cdef32703c8feb70a8a5
pimber <christofferctj@gmail.com>
2022-04-25 11:53:37 +0200

    Have done so that CharStream reads from file so larger exsamples can be tested on

--- compiler/src/App.java
+++ compiler/src/App.java
@@ -1,18 +1,16 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
-
 import antlr.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        CharStream input = CharStreams.fromString("num a = 7;");
+        CharStream input = CharStreams.fromFileName("src/sourceCode.txt");
         arcv2Lexer lexer = new arcv2Lexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         arcv2Parser parser = new arcv2Parser(tokens);
         ParseTree tree = parser.start();
         EvalVisitor eval = new EvalVisitor();
         eval.visit(tree);
-
         System.out.println("Hello, World!");
     }
 }
