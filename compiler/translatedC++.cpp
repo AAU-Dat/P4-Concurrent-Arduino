2e321827ca878d279b9ecc1c214fb06730540a70
Christoffer Trebbien Jønsson <christofferctj@gmail.com>
2022-05-20 13:32:59 +0200

    Fixed a small change in CodeGenVisitor where == transpiled to != and added some more text to integration test

--- compiler/translatedC++.cpp
+++ compiler/translatedC++.cpp
@@ -1,11 +1,62 @@
 #include "protothreads.h"
-float _b = 2 + 2;
-
-
-void setup(){
-Serial.begin(9600);
+float _buttonState = 0;
+float _switch = 0;
+#define _BUTTON_PIN 12
+#define _LED_PIN 11
+pt pt0;
+int pt0thread(struct pt *pt)
+{
+    PT_BEGIN(pt);
+    for (;;)
+    {
+        if (_switch == 0)
+        {
+            digitalWrite(_led_builtin, HIGH);
+            _switch = 1;
+        }
+        else
+        {
+            digitalWrite(_led_builtin, LOW);
+            _switch = 0;
+        }
+        PT_SLEEP(pt, 1000);
+    }
+    PT_END(pt);
+}
+float _blinkLED(float _buttonState)
+{
+    if (_buttonState == 1)
+    {
+        digitalWrite(_LED_PIN, HIGH);
+    }
+    else
+    {
+        digitalWrite(_LED_PIN, LOW);
+    }
+}
+pt pt1;
+int pt1thread(struct pt *pt)
+{
+    PT_BEGIN(pt);
+    for (;;)
+    {
+        _buttonState = digitalRead(_BUTTON_PIN);
+        _blinkLED();
+    }
+    PT_END(pt);
 }
 
-void loop(){
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(_BUTTON_PIN, INPUT);
+    pinMode(_LED_PIN, OUTPUT);
+    PT_INIT(&pt0);
+    PT_INIT(&pt1);
 }
 
+void loop()
+{
+    PT_SCHEDULE(pt0thread(&pt0));
+    PT_SCHEDULE(pt1thread(&pt1));
+}
