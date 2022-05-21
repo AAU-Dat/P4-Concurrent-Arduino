13b29e7e1aa231579b76de9489bab0ab882e9c69
Christoffer Trebbien Jønsson <christofferctj@gmail.com>
2022-05-21 11:48:26 +0200

    More fixed merge conflicts

--- compiler/translatedC++.cpp
+++ compiler/translatedC++.cpp
@@ -1,16 +1,10 @@
 #include "protothreads.h"
-<<<<<<< HEAD
-#define _LED_PIN 11
-#define _BUTTON_PIN 12
-float _buttonState = 0;
-bool _ledState = HIGH;
-=======
 float _buttonState = 0;
 float _switch = 0;
 #define _BUTTON_PIN 12
 #define _LED1_PIN 10
 #define _LED2_PIN 11
->>>>>>> 173694d2cd13e2e9aa9930d85edb9378329b196f
+
 pt pt0;
 int pt0thread(struct pt *pt)
 {
@@ -60,14 +54,9 @@ int pt1thread(struct pt *pt)
 void setup()
 {
     Serial.begin(9600);
-<<<<<<< HEAD
-    pinMode(_LED_PIN, OUTPUT);
-    pinMode(_BUTTON_PIN, OUTPUT);
-=======
     pinMode(_BUTTON_PIN, INPUT);
     pinMode(_LED1_PIN, OUTPUT);
     pinMode(_LED2_PIN, OUTPUT);
->>>>>>> 173694d2cd13e2e9aa9930d85edb9378329b196f
     PT_INIT(&pt0);
     PT_INIT(&pt1);
 }
